a3d7b9f73a307c842058b4fd3ecb90d05037449f
Sam Willis <sam.willis@gmail.com>
2026-06-23 07:50:51 +0800

    Admit text search dictionaries to threaded world

--- contrib/dict_int/dict_int.c
+++ contrib/dict_int/dict_int.c
@@ -17,7 +17,8 @@
 
 PG_MODULE_MAGIC_EXT(
 					.name = "dict_int",
-					.version = PG_VERSION
+					.version = PG_VERSION,
+					PG_MODULE_MAGIC_BACKEND_MODEL_THREAD_PER_SESSION
 );
 
 typedef struct
