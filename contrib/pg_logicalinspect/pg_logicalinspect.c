3a781713a07eada1122fce97819a1f26f32a5d13
Sam Willis <sam.willis@gmail.com>
2026-06-23 15:21:16 +0800

    Admit pg_logicalinspect metadata

--- contrib/pg_logicalinspect/pg_logicalinspect.c
+++ contrib/pg_logicalinspect/pg_logicalinspect.c
@@ -20,7 +20,8 @@
 
 PG_MODULE_MAGIC_EXT(
 					.name = "pg_logicalinspect",
-					.version = PG_VERSION
+					.version = PG_VERSION,
+					PG_MODULE_MAGIC_BACKEND_MODEL_THREAD_PER_SESSION
 );
 
 PG_FUNCTION_INFO_V1(pg_get_logical_snapshot_meta);
